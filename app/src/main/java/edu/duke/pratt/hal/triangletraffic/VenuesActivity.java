8b841dad89c1e5a3dcb7fe1f3c4b6b455e26bb7d
Ted Zhu <tedz2usa@gmail.com>
2015-06-08 16:43:40 -0400

    Add venues to table in VenuesActivity.

--- app/src/main/java/edu/duke/pratt/hal/triangletraffic/VenuesActivity.java
+++ app/src/main/java/edu/duke/pratt/hal/triangletraffic/VenuesActivity.java
@@ -4,14 +4,59 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import java.util.ArrayList;
 
 
 public class VenuesActivity extends ActionBarActivity {
+
+    private ArrayList<Venue> venues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_venue_list);
+
+        new DatabaseConnection(this);
+        
+        this.venues = Venue.asArrayList();
+
+        // Get reference to the Venue Table.
+        TableLayout tableLayout = (TableLayout) findViewById(R.id.venueTable);
+
+        // Populate the Venue Table with venue rows.
+        for (Venue venue : this.venues) {
+            TableRow row = this.getVenueRow(venue);
+            tableLayout.addView(row);
+        }
+
+    }
+
+    private TableRow getVenueRow(Venue venue) {
+
+        TableRow tableRow = new TableRow(this);
+
+        // Add ToggleButton.
+        ToggleButton toggle = new ToggleButton(this);
+        tableRow.addView(toggle);
+
+        // Add Name.
+        TextView name = new TextView(this);
+        name.setText(venue.getName());
+        tableRow.addView(name);
+
+        // Add Association.
+        TextView association = new TextView(this);
+        association.setText(venue.getAssociation());
+        tableRow.addView(association);
+
+
+        return tableRow;
     }
 
     @Override
