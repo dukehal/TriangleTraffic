856afc8839ee2bd88a93cc74f32d6e2283e03510
Ted Zhu <tedz2usa@gmail.com>
2015-06-17 10:58:16 -0400

    Renaming a class.

--- app/src/main/java/edu/duke/pratt/hal/triangletraffic/PreferenceConnection.java
+++ app/src/main/java/edu/duke/pratt/hal/triangletraffic/PreferenceConnection.java
@@ -4,12 +4,12 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
-import edu.duke.pratt.hal.triangletraffic.utility.TTPref;
+import edu.duke.pratt.hal.triangletraffic.utility.AppPref;
 
 public class PreferenceConnection {
 
     public PreferenceConnection(Context context) {
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
-        TTPref.setSharedPreferences(sharedPref);
+        AppPref.setSharedPreferences(sharedPref);
     }
 }
